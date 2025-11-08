4766a9d16792208f5bd09eab8d196cbb01a847b2
Danishveer Singh <danishveersingh2004@gmail.com>
2025-11-08 15:23:04 +0530

    add "bharat explorer"

--- frontend/src/components/StateDetails/StateDetail.jsx
+++ frontend/src/components/StateDetails/StateDetail.jsx
@@ -1,16 +1,55 @@
 import React, { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { motion, useScroll, useTransform } from "framer-motion";
 import statedata from "../../data/statedata.js";
 import "./StateDetail.css";
 
+const stateCodes = {
+  andaman_and_nicobar_island: "IN-AN",
+  andhra_pradesh: "IN-AP",
+  arunachal_pradesh: "IN-AR",
+  assam: "IN-AS",
+  bihar: "IN-BR",
+  chhattisgarh: "IN-CT",
+  delhi: "IN-DL",
+  goa: "IN-GA",
+  gujarat: "IN-GJ",
+  haryana: "IN-HR",
+  himachal_pradesh: "IN-HP",
+  jammu_and_kashmir: "IN-JK",
+  jharkhand: "IN-JH",
+  karnataka: "IN-KA",
+  kerala: "IN-KL",
+  lakshadweep_island: "IN-LD",
+  madhya_pradesh: "IN-MP",
+  maharashtra: "IN-MH",
+  manipur: "IN-MN",
+  meghalaya: "IN-ML",
+  mizoram: "IN-MZ",
+  nagaland: "IN-NL",
+  odisha: "IN-OR",
+  punjab: "IN-PB",
+  rajasthan: "IN-RJ",
+  sikkim: "IN-SK",
+  tamil_nadu: "IN-TN",
+  telangana: "IN-TG",
+  tripura: "IN-TR",
+  uttar_pradesh: "IN-UP",
+  uttarakhand: "IN-UT",
+  west_bengal: "IN-WB",
+};
+
 function StateDetail() {
   const { stateName, category } = useParams();
+  const navigate = useNavigate();
   const normalizedState = stateName?.toLowerCase().replace(/[\s-]+/g, "_");
   const stateInfo = statedata[normalizedState];
+  const stateCode = stateCodes[normalizedState];
 
   useEffect(() => {
-    if (!category) return;
+    if (!category) {
+      return;
+    }
     const body = document.body;
     body.classList.remove("culture-bg", "cuisine-bg", "sights-bg");
     if (category === "culture") {
@@ -28,12 +67,16 @@ function StateDetail() {
   const { text, images } = stateInfo[category];
   const textChunks = text.split("  ").map((chunk, i) => chunk.trim());
   const { scrollY } = useScroll();
-
-  // Check if mobile
   const isMobile = window.innerWidth <= 656;
 
   return (
     <>
+      <button
+        className="close-button"
+        onClick={() => navigate(`/map?state=${stateCode}`)}
+      >
+        <span className="close-icon">✖</span>
+      </button>
       <div className={`background-blur-bg ${category}-bg`}></div>
       <div className={`state-detail-container ${category}`}>
         <motion.h1
@@ -55,15 +98,13 @@ function StateDetail() {
         <div className="state-content">
           {textChunks.map((chunk, index) => {
             const scrollRange = 300;
-            const maxYMovement = isMobile ? 0 : 100; // Disable parallax on mobile
+            const maxYMovement = isMobile ? 0 : 100;
             const y = useTransform(
               scrollY,
               [index * scrollRange, (index + 1) * scrollRange],
               [maxYMovement, -maxYMovement]
             );
             const isEven = index % 2 === 0;
-
-            // Reduce animation distance on mobile
             const animationDistance = isMobile ? 50 : 500;
 
             return (
